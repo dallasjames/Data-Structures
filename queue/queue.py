0a75d224c326489a478d02b90a3da7d6a1c81937
Dallas James <dallasjames42@gmail.com>
2020-08-19 01:41:18 -0600

    finished queue with array

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,4 @@
+import array
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -18,15 +19,20 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = array.array('i', [])
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        self.storage = self.storage.append(value)
+        self.storage.append(value)
 
     def dequeue(self):
-        self.size -= 1
-        self.storage = self.storage.pop()
+        if len(self.storage) > 0:
+            dequeued = self.storage[0]
+            self.size -= 1
+            self.storage.remove(self.storage[0])
+            return dequeued
+        else:
+            return None
